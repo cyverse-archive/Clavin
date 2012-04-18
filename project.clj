bdb71e87b66b7ab1fb0098fe94212a54f5f22fee
Dennis Roberts <dennis@iplantcollaborative.org>
2012-04-17 17:39:02 -0700

    CORE-3104: use lein-iplant-rpm to generate RPM files

--- project.clj
+++ project.clj
@@ -1,8 +1,15 @@
 (defproject clavin "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :description "A command-line tool for loading service configurations into Zookeeper."
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.1"]]
+  :dev-dependencies [[org.iplantc/lein-iplant-rpm "1.1.0-SNAPSHOT"]]
+  :iplant-rpm {:summary "Clavin"
+               :type :command
+               :release 1
+               :provides "iplant-clavin"}
   :aot [clavin.core]
-  :main clavin.core)
+  :main clavin.core
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
