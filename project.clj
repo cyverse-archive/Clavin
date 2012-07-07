c0b4d44d7ddf73108f10c35fc98c13b6c1e197ac
Dennis Roberts <dennis@iplantcollaborative.org>
2012-07-06 17:34:47 -0700

    CORE-3342: upgrade to lein2 and the latest version of lein-iplant-rpm

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject clavin "1.0.0-SNAPSHOT"
+(defproject clavin "1.1.0-SNAPSHOT"
   :description "A command-line tool for loading service configurations into Zookeeper."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.1"]]
-  :dev-dependencies [[org.iplantc/lein-iplant-rpm "1.1.0-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.3.0-SNAPSHOT"]]
   :iplant-rpm {:summary "Clavin"
                :type :command
-               :release 1
                :provides "iplant-clavin"}
   :aot [clavin.core]
   :main clavin.core
