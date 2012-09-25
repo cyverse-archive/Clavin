c5a89b1d8d7348419c88cd581c51cdff50e2c80f
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-09-25 12:52:32 -0700

    Bumped the version on lein-iplant-rpm to 1.3.2.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/tools.cli "0.2.1"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.1"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.3.0-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.3.2-SNAPSHOT"]]
   :iplant-rpm {:summary "Clavin"
                :type :command
                :provides "iplant-clavin"}
