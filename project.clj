9e9bf582380a49b97f02f53582c81469b52326f8
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-31 14:33:26 -0700

    CORE-3929: switched to the latest version of iplant-clojure-commons

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.antlr/stringtemplate "4.0.2"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.1"]]
   :plugins [[org.iplantc/lein-iplant-rpm "1.3.2-SNAPSHOT"]]
   :iplant-rpm {:summary "Clavin"
