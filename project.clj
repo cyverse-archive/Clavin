bcabecaa18d33914375ab6e8d0076638320ba5f2
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-20 14:00:26 -0700

    upped dependencies and version
    clojure -> 1.5.1
    clojure-commons -> 1.4.1-SNAPSHOT
    lein-iplant-rpm -> 1.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clavin "1.2.1-SNAPSHOT"
+(defproject clavin "1.2.2-SNAPSHOT"
   :description "A command-line tool for loading service configurations into Zookeeper."
   :dependencies [[org.antlr/stringtemplate "4.0.2"]
-                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]
                  [zookeeper-clj "0.9.1"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.3.2-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"]]
   :iplant-rpm {:summary "Clavin"
                :type :command
                :provides "iplant-clavin"}
