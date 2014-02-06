33576e1836218cfbac743661a86a59fdc462638b
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-06 13:30:06 -0700

    CORE-5133: added some fields that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,16 +1,24 @@
-(defproject clavin "1.3.2-SNAPSHOT"
+(defproject org.iplantc/clavin "1.3.3-SNAPSHOT"
   :description "A command-line tool for loading service configurations into Zookeeper."
+  :url "http://www.iplantcollaborative.org"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/Clavin.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/Clavin.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/Clavin.git"}
   :dependencies [[org.antlr/stringtemplate "4.0.2"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.8-SNAPSHOT"]
                  [zookeeper-clj "0.9.1"]]
   :plugins [[org.iplantc/lein-iplant-cmdtar "0.1.2-SNAPSHOT"]
-            [org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"]]
+            [org.iplantc/lein-iplant-rpm "1.4.3-SNAPSHOT"]]
   :iplant-rpm {:summary "Clavin"
                :type :command
                :provides "iplant-clavin"}
   :aot [clavin.core]
   :main clavin.core
-  :repositories {"iplantCollaborative"
-                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
+  :deploy-repositories [["sonatype-nexus-staging"
+                         {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
