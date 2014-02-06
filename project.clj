a61c51facf8fc9e6cb64edbdf1da0165867e8bc4
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-06 13:43:54 -0700

    CORE-5133: removed snapshot dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/clavin "1.3.3-SNAPSHOT"
+(defproject org.iplantc/clavin "1.3.3"
   :description "A command-line tool for loading service configurations into Zookeeper."
   :url "http://www.iplantcollaborative.org"
   :license {:name "BSD"
@@ -9,10 +9,10 @@
   :dependencies [[org.antlr/stringtemplate "4.0.2"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.iplantc/clojure-commons "1.4.8-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.8"]
                  [zookeeper-clj "0.9.1"]]
-  :plugins [[org.iplantc/lein-iplant-cmdtar "0.1.2-SNAPSHOT"]
-            [org.iplantc/lein-iplant-rpm "1.4.3-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-cmdtar "0.1.2"]
+            [org.iplantc/lein-iplant-rpm "1.4.3"]]
   :iplant-rpm {:summary "Clavin"
                :type :command
                :provides "iplant-clavin"}
